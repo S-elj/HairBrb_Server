5c76c9b87d7ab815877f7af5805d1f03054a3876
Selj <samy.el-jaafari@etu.umontpellier.fr>
2024-05-01 15:58:13 +0200

    cors update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const usersRouter = require('./routes/users');
 const propertiesRouter = require('./routes/properties');
@@ -15,7 +17,7 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/users', usersRouter);
 app.use('/properties', propertiesRouter);
 app.use('/bookings', bookingsRouter);
